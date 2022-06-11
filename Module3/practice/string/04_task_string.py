4f46c42cb32801d71e55a33f41c4a850b37b3f89
Kenn0bi <106873868+Kenn0bi@users.noreply.github.com>
2022-06-11 19:07:26 +0500

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,11 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+split_text = text.split(" ")
+count = 0
+
+for word in split_text:
+    if len(word) > 5:
+        count += 1
+
+print("Слов из более 5 букв: ", count)
