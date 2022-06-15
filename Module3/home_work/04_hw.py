36751c264e853c73e889bc57eb2c7078692c4b62
Kenn0bi <106873868+Kenn0bi@users.noreply.github.com>
2022-06-15 14:07:54 +0500

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,29 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+# import random
+#
+# n = int(input("Введите целое положительное число: "))
+# numbers = []
+#
+# for i in range(1, n + 1):
+#     numbers.append(random.randint(-100, 100))
+
+numbers = [2, -5, 8, 9, -25, 25, 4]
+n = 7
+
+print(*numbers, sep=", ")
+new_numbers = []
+
+for i in range(0, n):
+    try:
+        if numbers[i] !=0 and (numbers[i] ** 0.5) % 1 == 0:
+            new_numbers.append(int(numbers[i] ** 0.5))
+    except:
+        pass
+
+if new_numbers != []:
+    print(*new_numbers, sep=", ")
+else:
+    print("Числа удовлетворяющие условию не найдены.")
