cda1b7b25f717ce129768899d44015de16aaae45
Kenn0bi <106873868+Kenn0bi@users.noreply.github.com>
2022-06-16 16:58:52 +0500

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -4,4 +4,20 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+first_number = int(input("Введите первое число: "))     # Первое число
+second_number = int(input("Введите второе число: "))    # Второе число
+
+reverse_list = False
+if first_number > second_number:
+    reverse_list = True
+    first_number, second_number = second_number, first_number
+
+num_divided_3 = []
+for i in range(first_number, second_number+1):
+    if i % 3 == 0:
+        num_divided_3.append(i)
+
+if reverse_list:
+    num_divided_3 = num_divided_3[::-1]
+
+print(*num_divided_3, sep=", ")
