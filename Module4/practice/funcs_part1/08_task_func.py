9bf1b2b63a438695124f6fd07c9c0eee31cc01fa
Kenn0bi <106873868+Kenn0bi@users.noreply.github.com>
2022-06-20 17:59:11 +0500

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,20 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+
+def fibonacci(n):
+    num_fib = 0
+    num_fib_prev = 0
+    i = 0
+
+    while i < n:
+        if i == 1:
+            num_fib = 1
+        else:
+            num_fib += num_fib_prev
+            num_fib_prev = num_fib - num_fib_prev
+        i += 1
+    return num_fib
+
+
+n = int(input("Введите целое положительное число: "))
+
+print(f"{n}-ое число Фибоначчи: {fibonacci(n)}")
