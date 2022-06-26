443eb6e227ac0e47592182346590e3d538559f4d
Andrey Shchegol <106873868+Kenn0bi@users.noreply.github.com>
2022-06-26 15:32:49 +0500

    Update 04_task_file.py

--- Module6/practice/files/04_task_file.py
+++ Module6/practice/files/04_task_file.py
@@ -11,3 +11,36 @@
 # Возможно пригодится:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+# Вариант 1
+"""
+Однократный проход файла fruits.txt, но многократное октрытие на дозапись файлов с фруктами по буквам
+"""
+alphabet = list(map(chr, range(ord('А'), ord('Я')+1)))
+
+with open("files/fruits.txt", "r", encoding="utf-8") as f_in:
+    for line in f_in:
+        fruit = line.strip()
+        if len(fruit) != 0 and fruit.upper()[0] in alphabet:
+            f_out_name = f"files/fruits_{fruit.upper()[0]}.txt"
+            with open(f_out_name, "a", encoding="utf-8") as f_out:
+                f_out.write(fruit + "\n")
+
+# Вариант 2
+"""
+Многократный проход файла fruits.txt (кратно количеству букв в алфавите), но однократное открытие файлов на запись
+"""
+alphabet = list(map(chr, range(ord('А'), ord('Я')+1)))
+
+for letter in alphabet:
+    with open("files/fruits.txt", "r", encoding="utf-8") as f_in:
+        fruits = []
+        for line in f_in:
+            fruit = line.strip()
+            if len(fruit) != 0 and fruit.upper()[0] == letter:
+                fruits.append(fruit)
+        if len(fruits) !=0:
+            f_out_name = f"files/fruits_{letter}.txt"
+            with open(f_out_name, "w", encoding="utf-8") as f_out:
+                for fruit in fruits:
+                    f_out.write(fruit + "\n")
